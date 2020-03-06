f8cca4ffb7d823dc687294468bcc17c297a139c8
Nam Do <namdo@csm-wl-dhcp-78-6.mines.edu>
2020-03-06 16:08:37 -0700

    Login added

--- app/src/main/java/com/csci448/RealTime/FinalProject/MainActivity.kt
+++ app/src/main/java/com/csci448/RealTime/FinalProject/MainActivity.kt
@@ -2,11 +2,16 @@ package com.csci448.RealTime.FinalProject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csci448.RealTime.FinalProject.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val currentFragment =supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(currentFragment == null ) {
+            val fragment = LoginFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }    }
 }
--- app/src/main/java/com/csci448/RealTime/FinalProject/ui/login/LoginFragment.kt
+++ app/src/main/java/com/csci448/RealTime/FinalProject/ui/login/LoginFragment.kt
@@ -1,4 +1,19 @@
 package com.csci448.RealTime.FinalProject.ui.login
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.csci448.RealTime.FinalProject.R
+
+class LoginFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view=inflater.inflate(R.layout.activity_login,container,false)
+        return view
+    }
 }
