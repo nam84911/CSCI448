05541880220dff333541601d9488a87e16c74dc0
dkoMines <dko@mymail.mines.edu>
2020-04-28 21:51:41 -0600

    Moved cam's location intent to be with mine w/ proper setting

--- app/src/main/java/com/csci448/RealTime/FinalProject/ui/detail/ActivityDetailFragment.kt
+++ app/src/main/java/com/csci448/RealTime/FinalProject/ui/detail/ActivityDetailFragment.kt
@@ -177,15 +177,7 @@ class ActivityDetailFragment : Fragment(){
                     activityDetailViewModel.addActivity(activity, day)
                     TimePickerFragment.reset()
                     TimePickerFragmentWake.reset()
-                    setAlarms()
-                    val intent=Intent(requireContext(),LocationReceiver::class.java)
-                    intent.putExtra(LocationReceiver.LONG,activity.long)
-                    intent.putExtra(LocationReceiver.LAT,activity.lat)
-                    intent.putExtra(LocationReceiver.UUID,activity.uuid)
-
-                    val pendingIntent:PendingIntent=PendingIntent.getBroadcast(requireActivity(),activity.hr+activity.min+activity.arr_hr,intent,PendingIntent.FLAG_UPDATE_CURRENT)
-                    val alarmManager:AlarmManager=requireActivity().getSystemService(ALARM_SERVICE) as AlarmManager
-                    alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+10,pendingIntent)
+                    setAlarms(activity)
                     callbacks?.returnScreen()
                 } else {
                     val t = Toast.makeText(context,"Please select a day",Toast.LENGTH_SHORT)
@@ -321,10 +313,10 @@ class ActivityDetailFragment : Fragment(){
     /**
      * Creates our alarms.
      */
-    fun setAlarms(){
+    fun setAlarms(activity : Activity){
         val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
         val i : Intent = Intent(context,AlarmReciever::class.java)
-        i.putExtra("ADDRESS",addressButton.text.toString())
+        i.putExtra("ADDRESS",activity.address)
         val alarmIntent = PendingIntent.getBroadcast(context,0, i,0)
         val c = Calendar.getInstance()
         c.set(Calendar.HOUR_OF_DAY,TimePickerFragmentWake.hr)
@@ -346,10 +338,10 @@ class ActivityDetailFragment : Fragment(){
         alarmManager?.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, c.timeInMillis,alarmIntent)
 //        alarmManager?.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+3*1000,alarmIntent)
 
-        val locationAlarmManager = context?.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
-        val locationFind = Intent(context,LocationReceiver::class.java).let { intent ->
-            PendingIntent.getBroadcast(context,0, intent,0)
-        }
+//        val locationAlarmManager = context?.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+//        val locationFind = Intent(context,LocationReceiver::class.java).let { intent ->
+//            PendingIntent.getBroadcast(context,0, intent,0)
+//        }
         val c2 = Calendar.getInstance()
         c2.set(Calendar.HOUR_OF_DAY,TimePickerFragment.hr)
         c2.set(Calendar.MINUTE,TimePickerFragment.min)
@@ -367,8 +359,16 @@ class ActivityDetailFragment : Fragment(){
             }
         }
         if (c2.before(Calendar.getInstance())) {c2.add(Calendar.DATE,7)}
-        locationAlarmManager?.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, c.timeInMillis,locationFind)
+//        locationAlarmManager?.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, c.timeInMillis,locationFind)
 //        locationAlarmManager?.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+3*1000,locationFind)
+        val intent=Intent(requireContext(),LocationReceiver::class.java)
+        intent.putExtra(LocationReceiver.LONG,activity.long)
+        intent.putExtra(LocationReceiver.LAT,activity.lat)
+        intent.putExtra(LocationReceiver.UUID,activity.uuid)
+
+        val pendingIntent:PendingIntent=PendingIntent.getBroadcast(requireActivity(),activity.hr+activity.min+activity.arr_hr,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        val locationManager:AlarmManager=requireActivity().getSystemService(ALARM_SERVICE) as AlarmManager
+        locationManager.set(AlarmManager.RTC_WAKEUP,c2.timeInMillis,pendingIntent)
 
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
